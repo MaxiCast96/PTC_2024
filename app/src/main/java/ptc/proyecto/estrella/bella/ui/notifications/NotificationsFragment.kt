341bad1571be0132933f44b546932b226411657d
PeLima9 <eduardolima102a15@gmail.com>
2024-07-11 15:36:21 -0600

    Empezando el CRUD de Perfil

--- app/src/main/java/ptc/proyecto/estrella/bella/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/ptc/proyecto/estrella/bella/ui/notifications/NotificationsFragment.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import modelo.ClaseConexion
+import modelo.listaUsuarios
+import ptc.proyecto.estrella.bella.R
 import ptc.proyecto.estrella.bella.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,7 +39,22 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val btnEditUser = root.findViewById<Button>(R.id.btnEditUser)
+        val btnEditName = root.findViewById<Button>(R.id.btnEditName)
+        val btnEditPayment = root.findViewById<Button>(R.id.btnDetalleFacturaEdit)
 
+        TODO("ARREGLAR ESTA MADRE")
+        btnEditUser.setOnClickListener{
+            //Edit User
+            val context = this.context
+
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Editar Usuario")
+
+            val textBox = EditText(context)
+            textBox.setHint(listaUsuarios.nombre)
+
+        }
         return root
     }
 
