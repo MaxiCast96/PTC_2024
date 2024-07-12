d6e0d44f3cbe1f1a8e72b5d2d35c0ae560bebc6b
Dannyce27 <>
2024-07-12 15:42:30 -0600

    setOnClickListener del fragmento Home

--- app/src/main/java/ptc/proyecto/estrella/bella/activity_pago.kt
+++ app/src/main/java/ptc/proyecto/estrella/bella/activity_pago.kt
@@ -1,29 +1,20 @@
 package ptc.proyecto.estrella.bella
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class detalle_horarios : AppCompatActivity() {
+class activity_pago : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detalle_horarios)
+        setContentView(R.layout.activity_pago)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val imgAtras = findViewById<ImageView>(R.id.imgAtras)
-
-        imgAtras.setOnClickListener {
-            val intent = Intent(this, MainActivity ::class.java)
-            startActivity(intent)
         }
     }
 }
--- app/src/main/java/ptc/proyecto/estrella/bella/ui/home/HomeFragment.kt
+++ app/src/main/java/ptc/proyecto/estrella/bella/ui/home/HomeFragment.kt
@@ -1,13 +1,18 @@
 package ptc.proyecto.estrella.bella.ui.home
 
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import ptc.proyecto.estrella.bella.R
 import ptc.proyecto.estrella.bella.databinding.FragmentHomeBinding
+import ptc.proyecto.estrella.bella.detalle_horarios
 
 class HomeFragment : Fragment() {
 
@@ -27,6 +32,98 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+//todo el codigo aqui
+
+        val imgIntensamente = root.findViewById<ImageView>(R.id.imgIntensamente)
+        val imgUp = root.findViewById<ImageView>(R.id.imgUp)
+        val imgMarioBros = root.findViewById<ImageView>(R.id.imgMarioBros)
+        val imgMonsterInc = root.findViewById<ImageView>(R.id.imgMonsterInc)
+        val imgVenomLetThereBeCarnage = root.findViewById<ImageView>(R.id.imgVenomLetThereBeCarnage)
+        val imgVenom = root.findViewById<ImageView>(R.id.imgVenom)
+        val imgAcrossTheSpiderverse = root.findViewById<ImageView>(R.id.imgAcrossTheSpiderverse)
+        val imgIntoTheSpiderverse = root.findViewById<ImageView>(R.id.imgIntoTheSpiderverse)
+
+
+        imgIntensamente.setOnClickListener {
+            val intent = Intent(requireContext(), detalle_horarios::class.java)
+
+            intent.putExtra(
+                "imagenIntensamente"
+                item.imagenIntensamente
+            )
+            startActivity(intent)
+        }
+
+        imgUp.setOnClickListener {
+            val intent = Intent(requireContext(), detalle_horarios::class.java)
+            intent.putExtra(
+                "imagenUp"
+                        item.imagenUp
+            )
+            startActivity(intent)
+        }
+
+
+        imgMarioBros.setOnClickListener {
+            val intent = Intent(requireContext(), detalle_horarios::class.java)
+            intent.putExtra(
+                "imagenMarioBros"
+                        item.imagenMarioBros
+            )
+            startActivity(intent)
+        }
+
+        imgMonsterInc.setOnClickListener {
+            val intent = Intent(requireContext(), detalle_horarios::class.java)
+            intent.putExtra(
+                "imagenMonsterInc"
+                        item.imagenMonsterInc
+            )
+            startActivity(intent)
+        }
+
+        imgVenomLetThereBeCarnage.setOnClickListener {
+            val intent = Intent(requireContext(), detalle_horarios::class.java)
+            intent.putExtra(
+                "imagenVenomLetThereBeCarnage"
+                        item.imagenVenomLetThereBeCarnage
+            )
+            startActivity(intent)
+        }
+
+        imgVenom.setOnClickListener {
+            val intent = Intent(requireContext(), detalle_horarios::class.java)
+            intent.putExtra(
+                "imagenVenom"
+                        item.imagenVenom
+            )
+            startActivity(intent)
+        }
+
+        imgAcrossTheSpiderverse.setOnClickListener {
+            val intent = Intent(requireContext(), detalle_horarios::class.java)
+            intent.putExtra(
+                "imagenAcrossTheSpiderverse"
+                        item.imagenAcrossTheSpiderverse
+            )
+            startActivity(intent)
+        }
+
+        imgIntoTheSpiderverse.setOnClickListener {
+            val intent = Intent(requireContext(), detalle_horarios::class.java)
+            intent.putExtra(
+                "imagenIntoTheSpiderverse"
+                        item.imagenIntoTheSpiderverse
+            )
+            startActivity(intent)
+        }
+
+
+
+
+
+
 
 
         return root
