b9b35a90df775b0786013594368bc6a2839101da
Nightmare <lfernandonaleman@gmail.com>
2024-07-14 17:37:28 -0600

    ven y sana mi dolor 2.0

--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -6,7 +6,6 @@ import java.sql.DriverManager
 class ClaseConexion {
     fun cadenaConexion(): Connection? {
         try {
-<<<<<<< HEAD
 
        val ip = "jdbc:oracle:thin:@192.168.0.9:1521:xe"
         val usuario = "Ragnarok"
@@ -16,18 +15,9 @@ class ClaseConexion {
         return conexion
             }catch (e: Exception){
                 println("El error es este: $e")
-=======
-            val ip = "jdbc:oracle:thin:@192.168.1.17:1521:xe"
-            val usuario = "GUILLE"
-            val contrasena = "fortnite_201"
-
-            val conexion = DriverManager.getConnection(ip, usuario, contrasena)
-            return conexion
-
-        }catch (e: Exception){
-            println("El error es este: $e")
->>>>>>> main
             return null
             }
+
+
     }
 }
