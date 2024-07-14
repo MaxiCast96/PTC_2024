4f8f41ae43db25215c7421737a37286f968d8368
PeLima9 <eduardolima102a15@gmail.com>
2024-07-14 17:20:51 -0600

    Avances en el SELECT

---
+++ app/src/main/java/ptc/proyecto/estrella/bella/edit_account_details.kt
@@ -0,0 +1,20 @@
+package ptc.proyecto.estrella.bella
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class edit_account_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edit_account_details)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/ptc/proyecto/estrella/bella/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/ptc/proyecto/estrella/bella/ui/notifications/NotificationsViewModel.kt
@@ -3,11 +3,15 @@ package ptc.proyecto.estrella.bella.ui.notifications
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import modelo.listaUsuarios
 
-class NotificationsViewModel : ViewModel() {
+class NotificationsViewModel() : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
     }
+
     val text: LiveData<String> = _text
 }
